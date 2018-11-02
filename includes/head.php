94aa2ce0d650745b240549d5e4a5acead1dc9730
RodickColonel <38560874+RodickColonel@users.noreply.github.com>
2018-11-02 00:38:43 +0000

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,1 +1,24 @@
-       <meta charset="utf-8">    <meta name="language" content="english">     <meta http-equiv="content-type" content="text/html">    <meta name="author" content="Radek kuhnel">    <meta name="publisher" content="https://lookforplace.co.uk">    <meta name="description" content="Form example using html5 validations">    <meta name="keywords" content="html5,tags,input,form,submit">    <meta name="web_author" content="Rafayel Mkrtchyan">    <meta name="copyright" content="Copyright 2018">    <meta name="reply-to" content="radekkuhnel@hotmail.com">    <meta name="city" content="London">    <meta name="country" content="United Kingdom">      <meta name="format-detection" content="telephone=yes">    <meta name="HandheldFriendly" content="true">     <meta name="viewport" content="width=device-width, initial-scale=1.0">        <meta http-equiv="Content-Style-Type" content="text/css">    <meta http-equiv="Content-Script-Type" content="text/javascript">        <title>Form examples</title>    <link rel="stylesheet" type="text/css" href="style.css">    
+   
+    <meta charset="utf-8">
+    <meta name="language" content="english"> 
+    <meta http-equiv="content-type" content="text/html">
+    <meta name="author" content="Radek kuhnel">
+    <meta name="publisher" content="https://lookforplace.co.uk">
+    <meta name="description" content="Form example using html5 validations">
+    <meta name="keywords" content="html5,tags,input,form,submit">
+    <meta name="web_author" content="Rafayel Mkrtchyan">
+    <meta name="copyright" content="Copyright 2018">
+    <meta name="reply-to" content="radekkuhnel@hotmail.com">
+    <meta name="city" content="London">
+    <meta name="country" content="United Kingdom">
+  
+    <meta name="format-detection" content="telephone=yes">
+    <meta name="HandheldFriendly" content="true"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta http-equiv="Content-Style-Type" content="text/css">
+    <meta http-equiv="Content-Script-Type" content="text/javascript">
+    
+    <title>Form examples</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
